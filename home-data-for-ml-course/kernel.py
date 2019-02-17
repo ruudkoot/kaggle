94b7087c4884fcb7ede091f3a1d0fe747ea3ba36
Ruud Koot <inbox@ruudkoot.nl>
2019-02-17 16:32:22 +0000

    n_est=1600 lr=0.05

--- home-data-for-ml-course/kernel.py
+++ home-data-for-ml-course/kernel.py
@@ -32,7 +32,7 @@ home_X = my_imputer.fit_transform(home_X_copy)
 test_X = my_imputer.fit_transform(test_X_copy)
 
 #rf_model_on_full_data = RandomForestRegressor(n_estimators=924)
-rf_model_on_full_data = XGBRegressor(n_estimators=460)
+rf_model_on_full_data = XGBRegressor(n_estimators=1600,learning_rate=0.05,n_jobs=4)
 rf_model_on_full_data.fit(home_X,home_y)
 
 # make predictions which we will submit.
@@ -43,26 +43,28 @@ output.to_csv('submission.csv', index=False)
 
 print("Done!")
 
-#########################
+######################### hyper-parameter tuning
 
 train_X, val_X, train_y, val_y = train_test_split(home_X, home_y, random_state=1)
 
-def tune(n_est):
-    rf_model = XGBRegressor(n_estimators=n_est,random_state=1)
+def tune(n_est,lr):
+    rf_model = XGBRegressor(n_estimators=n_est,learning_rate=lr,random_state=1,n_jobs=4)
     rf_model.fit(train_X, train_y)
     rf_val_predictions = rf_model.predict(val_X)
     rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
     return rf_val_mae
 
-min_n_est,min_mae=None,None
-for n_est in range(450,10000,1):
-    mae=tune(n_est)
-    if min_mae == None or mae < min_mae:
-        min_n_est,min_mae = n_est,mae
-        print(min_n_est,min_mae)
-    else:
-        print(n_est,'-----')
-print(min_n_est,min_mae)
+min_n_est,min_lr,min_mae=None,None,None
+for n_est in range(100,10000,100):
+    for lri in range(1,11,1):
+        lr=float(lri)/100
+        mae=tune(n_est,lr)
+        if min_mae == None or min_lr == None or mae < min_mae:
+            min_n_est,min_lr,min_mae = n_est,lr,mae
+            print(min_n_est,min_lr,min_mae)
+        else:
+            print(n_est,lr,'-----')
+print(min_n_est,min_lr,min_mae)
 rf_val_mae=min_mae
 
 print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
